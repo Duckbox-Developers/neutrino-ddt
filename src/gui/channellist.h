19b525bb66c4847d911231a9ed86ff6dfe0a4b03
martii <m4rtii@gmx.de>
2014-05-03 18:21:48 +0200

    gui/channellist: display event list asynchronously
    Conflicts:
    	src/gui/channellist.cpp

--- src/gui/channellist.h
+++ src/gui/channellist.h
@@ -115,6 +115,7 @@ private:
 	int			infozone_width;
 	int			infozone_height;
 	int			previous_channellist_additional;
+	int			paint_events_index;
 
 	const char *		unit_short_minute;
 
@@ -145,8 +146,9 @@ private:
 	std::string   MaxChanNr();
 	void paintPig(int x, int y, int w, int h);
 	void paint_events(int index);
-	CChannelEventList	evtlist;
-	void readEvents(const t_channel_id channel_id);
+	void paint_events();
+	static void *paint_events(void *arg);
+	void readEvents(const t_channel_id channel_id, CChannelEventList &evtlist);
 	void showdescription(int index);
 	typedef std::pair<std::string,int> epg_pair;
 	std::vector<epg_pair> epgText;
