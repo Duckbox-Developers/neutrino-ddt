83903401dd4ae9c49647be5139d553b478d25f72
max10 <max_10@gmx.de>
2014-11-30 23:47:47 +0100

    fix TS

--- src/gui/movieplayer.cpp
+++ src/gui/movieplayer.cpp
@@ -249,8 +249,8 @@ void CMoviePlayerGui::restoreNeutrino()
 	if (isUPNP)
 		return;
 
-	//g_Zapit->unlockPlayBack();
-	CZapit::getInstance()->EnablePlayback(true);
+	g_Zapit->unlockPlayBack();
+	//CZapit::getInstance()->EnablePlayback(true);
 	g_Sectionsd->setPauseScanning(false);
 
 	printf("%s: restore mode %x\n", __func__, m_LastMode);fflush(stdout);
