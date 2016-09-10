23806d25a49bf9e03fcaf2bad2f3b7c5730a5b62
TangoCash <eric@loxat.de>
2016-09-10 15:24:55 +0200

    fix after merge

--- src/gui/movieplayer.cpp
+++ src/gui/movieplayer.cpp
@@ -1306,7 +1306,7 @@ bool CMoviePlayerGui::PlayFileStart(void)
 #ifdef ENABLE_GRAPHLCD
 	nGLCD::MirrorOSD(false);
 	if (p_movie_info)
-		nGLCD::lockChannel(p_movie_info->epgChannel, p_movie_info->epgTitle);
+		nGLCD::lockChannel(p_movie_info->channelName, p_movie_info->epgTitle);
 	else {
 		glcd_play = true;
 		nGLCD::lockChannel(g_Locale->getText(LOCALE_MOVIEPLAYER_HEAD), file_name.c_str(), file_prozent);
@@ -1489,7 +1489,7 @@ void CMoviePlayerGui::PlayFileLoop(void)
 	{
 #ifdef ENABLE_GRAPHLCD
 		if (p_movie_info)
-			nGLCD::lockChannel(p_movie_info->epgChannel, p_movie_info->epgTitle, duration ? (100 * position / duration) : 0);
+			nGLCD::lockChannel(p_movie_info->channelName, p_movie_info->epgTitle, duration ? (100 * position / duration) : 0);
 		else {
 			glcd_play = true;
 			nGLCD::lockChannel(g_Locale->getText(LOCALE_MOVIEPLAYER_HEAD), file_name.c_str(), file_prozent);
@@ -2060,7 +2060,7 @@ void CMoviePlayerGui::callInfoViewer(bool init_vzap_it)
 			p_movie_info = &movie_info;
 #ifdef ENABLE_GRAPHLCD
 		if (p_movie_info)
-			nGLCD::lockChannel(p_movie_info->epgChannel, p_movie_info->epgTitle);
+			nGLCD::lockChannel(p_movie_info->channelName, p_movie_info->epgTitle);
 #endif
 	}
 
