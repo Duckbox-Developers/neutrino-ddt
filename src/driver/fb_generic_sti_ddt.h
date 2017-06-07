22237d2f869b336b392a2636324815b607127f0b
TangoCash <eric@loxat.de>
2017-06-07 20:57:13 +0200

    fix after merge

--- src/driver/fb_generic_sti_ddt.h
+++ src/driver/fb_generic_sti_ddt.h
@@ -298,6 +298,7 @@ class CFrameBuffer : public sigc::trackable
 		void blitBPA2FB(unsigned char *mem, SURF_FMT fmt, int w, int h, int x = 0, int y = 0, int pan_x = -1, int pan_y = -1, int fb_x = -1, int fb_y = -1, int fb_w = -1, int fb_h = -1, int transp = false);
 		bool needAlign4Blit() { return false; };
 		uint32_t getWidth4FB_HW_ACC(const uint32_t x, const uint32_t w, const bool max=true);
+		int scale2Res(int size) {return size; };
 
 
 // ## AudioMute / Clock ######################################
