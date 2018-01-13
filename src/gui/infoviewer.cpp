ab8f1037e0508ef697cf856248cf68eeae750083
Frankenstone <dampf_acc@yahoo.com>
2018-01-13 19:23:56 +0100

    infoviewer: fix indentation in setInfobarTimeout

--- src/gui/infoviewer.cpp
+++ src/gui/infoviewer.cpp
@@ -914,16 +914,19 @@ void CInfoViewer::setInfobarTimeout(int timeout_ext)
 	{
 		case NeutrinoModes::mode_radio:
 		case NeutrinoModes::mode_webradio:
-				timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR_RADIO] + timeout_ext);
-				break;
+			timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR_RADIO] + timeout_ext);
+			break;
 		case NeutrinoModes::mode_ts:
+			if (CMoviePlayerGui::getInstance().IsAudioPlaying())
+				timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR_RADIO] + timeout_ext);
+			else
 				timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR_MOVIE] + timeout_ext);
-				break;
+			break;
 		case NeutrinoModes::mode_tv:
 		case NeutrinoModes::mode_webtv:
 		default:
-				timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR] + timeout_ext);
-				break;
+			timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR] + timeout_ext);
+			break;
 	}
 }
 
