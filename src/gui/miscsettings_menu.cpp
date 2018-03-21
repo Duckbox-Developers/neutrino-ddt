872f17696708e0d96dce430e59b7648c2bd8f040
max_10 <max_10@gmx.de>
2018-03-21 23:28:26 +0100

    armbox: activate kerneloptions

--- src/gui/miscsettings_menu.cpp
+++ src/gui/miscsettings_menu.cpp
@@ -43,7 +43,7 @@
 #include <gui/plugins.h>
 #include <gui/sleeptimer.h>
 #include <gui/zapit_setup.h>
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 #include <gui/kerneloptions.h>
 #endif
 
@@ -342,7 +342,7 @@ int CMiscMenue::showMiscSettingsMenu()
 	mf->setHint("", LOCALE_MENU_HINT_MISC_CPUFREQ);
 	misc_menue.addItem(mf);
 #endif /*CPU_FREQ*/
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 	// kerneloptions
 	CKernelOptions kernelOptions;
 	mf = new CMenuForwarder(LOCALE_KERNELOPTIONS_HEAD, true, NULL, &kernelOptions, NULL, CRCInput::RC_6);
