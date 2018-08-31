8b4e004256de31e3bd6f73e96451a73e2e195cee
TangoCash <eric@loxat.de>
2018-08-31 20:49:47 +0200

    add option to read (online) xmltv epg

--- lib/sectionsdclient/sectionsdclient.cpp
+++ lib/sectionsdclient/sectionsdclient.cpp
@@ -196,6 +196,14 @@ void CSectionsdClient::readSIfromXML(const char * epgxmlname)
 	close_connection();
 }
 
+void CSectionsdClient::readSIfromIPTVXML(const char * url)
+{
+	send(sectionsd::readSIfromIPTVXML, (char*) url, strlen(url));
+
+	readResponse();
+	close_connection();
+}
+
 void CSectionsdClient::writeSI2XML(const char * epgxmlname)
 {
 	send(sectionsd::writeSI2XML, (char*) epgxmlname, strlen(epgxmlname));
