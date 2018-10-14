e6986caa2af6c3965b6e3101b887dc1f369c783e
vanhofen <vanhofen@gmx.de>
2018-10-14 22:37:03 +0200

    - cec-setup: fix item mode when switching cec on/off

--- src/gui/cec_setup.cpp
+++ src/gui/cec_setup.cpp
@@ -62,6 +62,7 @@ CCECSetup::CCECSetup()
 	width = 40;
 	cec1 = NULL;
 	cec2 = NULL;
+	cec3 = NULL;
 }
 
 CCECSetup::~CCECSetup()
@@ -198,6 +199,7 @@ bool CCECSetup::changeNotify(const neutrino_locale_t OptionName, void * /*data*/
 		printf("[neutrino CEC Settings] %s set CEC settings...\n", __FUNCTION__);
 		cec1->setActive(g_settings.hdmi_cec_mode != VIDEO_HDMI_CEC_MODE_OFF);
 		cec2->setActive(g_settings.hdmi_cec_mode != VIDEO_HDMI_CEC_MODE_OFF);
+		cec3->setActive(g_settings.hdmi_cec_mode != VIDEO_HDMI_CEC_MODE_OFF);
 		videoDecoder->SetCECMode((VIDEO_HDMI_CEC_MODE)g_settings.hdmi_cec_mode);
 	}
 	else if (ARE_LOCALES_EQUAL(OptionName, LOCALE_VIDEOMENU_HDMI_CEC_STANDBY))
