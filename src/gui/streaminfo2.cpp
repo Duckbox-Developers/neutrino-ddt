0c4882a9117254d3a349ebaf804e6b1817c0fd3e
Knicko <knicko@glj.servebbs.com>
2018-11-15 22:35:20 +0100

    - streaminfo2: change name Picon to Logo

--- src/gui/streaminfo2.cpp
+++ src/gui/streaminfo2.cpp
@@ -1067,10 +1067,10 @@ void CStreamInfo2::paint_techinfo(int xpos, int ypos)
 		r.key = r.val = "";
 		v.push_back(r);
 
-		// picon
+		// channellogo
 		if (CNeutrinoApp::getInstance()->getMode() == NeutrinoModes::mode_webtv || CNeutrinoApp::getInstance()->getMode() == NeutrinoModes::mode_webradio)
 		{
-			r.key = "Picon";
+			r.key = "Logo";
 			r.key += ": ";
 			snprintf(buf, sizeof(buf), "%llx.png", channel->getChannelID() & 0xFFFFFFFFFFFFULL);
 			r.val = buf;
@@ -1113,8 +1113,8 @@ void CStreamInfo2::paint_techinfo(int xpos, int ypos)
 		r.key = r.val = "";
 		v.push_back(r);
 
-		// picon
-		r.key = "Picon";
+		// channellogo
+		r.key = "Logo";
 		r.key += ": ";
 		snprintf(buf, sizeof(buf), "%llx.png", channel->getChannelID() & 0xFFFFFFFFFFFFULL);
 		r.val = buf;
