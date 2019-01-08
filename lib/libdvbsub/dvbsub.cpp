339b8dc968fcffa7ab756c0c55759df2851a3977
TangoCash <eric@loxat.de>
2019-01-08 20:15:48 +0100

    activate subtitles

--- lib/libdvbsub/dvbsub.cpp
+++ lib/libdvbsub/dvbsub.cpp
@@ -15,7 +15,7 @@
 
 #include <poll.h>
 
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 extern "C" {
 #include <ass/ass.h>
 }
@@ -34,14 +34,14 @@ extern "C" {
 Debug sub_debug;
 static PacketQueue packet_queue;
 static PacketQueue bitmap_queue;
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 static PacketQueue ass_queue;
 static sem_t ass_sem;
 #endif
 
 static pthread_t threadReader;
 static pthread_t threadDvbsub;
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 static pthread_t threadAss = 0;
 #endif
 
@@ -50,7 +50,7 @@ static pthread_mutex_t readerMutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t packetCond = PTHREAD_COND_INITIALIZER;
 static pthread_mutex_t packetMutex = PTHREAD_MUTEX_INITIALIZER;
 
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 static OpenThreads::Mutex ass_mutex;
 static std::map<int,ASS_Track*> ass_map;
 static ASS_Library *ass_library = NULL;
@@ -59,22 +59,22 @@ static ASS_Track *ass_track = NULL;
 #endif
 
 static int reader_running;
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 static int ass_reader_running;
 #endif
 static int dvbsub_running;
 static int dvbsub_paused = true;
-static int dvbsub_pid = 0;
+static int dvbsub_pid;
 static int dvbsub_stopped;
 static int pid_change_req;
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 static bool isEplayer = false;
 #endif
 
 cDvbSubtitleConverter *dvbSubtitleConverter;
 static void* reader_thread(void *arg);
 static void* dvbsub_thread(void* arg);
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 static void* ass_reader_thread(void *arg);
 #endif
 static void clear_queue();
@@ -104,7 +104,7 @@ int dvbsub_init() {
 		return -1;
 	}
 
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 	ass_reader_running = true;
 	sem_init(&ass_sem, 0, 0);
 	trc = pthread_create(&threadAss, 0, ass_reader_thread, NULL);
@@ -127,7 +127,7 @@ int dvbsub_pause()
 
 		printf("[dvb-sub] paused\n");
 	}
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 	OpenThreads::ScopedLock<OpenThreads::Mutex> m_lock(ass_mutex);
 	ass_track = NULL;
 #endif
@@ -135,21 +135,25 @@ int dvbsub_pause()
 	return 0;
 }
 
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 int dvbsub_start(int pid, bool _isEplayer)
 #else
 int dvbsub_start(int pid)
 #endif
 {
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 	isEplayer = _isEplayer;
 	if (isEplayer && !dvbsub_paused)
 		return 0;
+	if (!isEplayer && !pid)
+#else
+	if (!pid)
 #endif
-	if(!dvbsub_paused && !pid)
+		pid = -1;
+	if(!dvbsub_paused && (pid < 0))
 		return 0;
 
-	if(pid && (pid != dvbsub_pid)) {
+	if(pid > -1 && pid != dvbsub_pid) {
 		dvbsub_pause();
 		if(dvbSubtitleConverter)
 			dvbSubtitleConverter->Reset();
@@ -157,8 +161,8 @@ int dvbsub_start(int pid)
 		pid_change_req = 1;
 	}
 printf("[dvb-sub] start, stopped %d pid %x\n", dvbsub_stopped, dvbsub_pid);
-	if(dvbsub_pid > 0) {
-#if HAVE_SH4_HARDWARE
+	if(dvbsub_pid > -1) {
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 		if (isEplayer) {
 			OpenThreads::ScopedLock<OpenThreads::Mutex> m_lock(ass_mutex);
 			std::map<int,ASS_Track*>::iterator it = ass_map.find(dvbsub_pid);
@@ -175,7 +179,7 @@ printf("[dvb-sub] start, stopped %d pid %x\n", dvbsub_stopped, dvbsub_pid);
 		pthread_mutex_lock(&readerMutex);
 		pthread_cond_broadcast(&readerCond);
 		pthread_mutex_unlock(&readerMutex);
-		printf("[dvb-sub] started with pid 0x%x\n", dvbsub_pid);
+		printf("[dvb-sub] started with pid 0x%x\n", pid);
 	}
 
 	return 1;
@@ -185,7 +189,7 @@ static int flagFd = -1;
 
 int dvbsub_stop()
 {
-	dvbsub_pid = 0;
+	dvbsub_pid = -1;
 	if(reader_running) {
 		dvbsub_stopped = 1;
 		dvbsub_pause();
@@ -203,10 +207,16 @@ int dvbsub_getpid()
 
 void dvbsub_setpid(int pid)
 {
-	if(!pid)
-		return;
-
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
+	if (!isEplayer && !pid)
+#else
+	if (!pid)
+#endif
+		pid = -1;
 	dvbsub_pid = pid;
+
+	if(dvbsub_pid < 0)
+		return;
 
 	clear_queue();
 
@@ -248,7 +258,7 @@ int dvbsub_close()
 		pthread_detach(threadDvbsub);
 		threadDvbsub = 0;
 	}
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 	if (ass_reader_running) {
 		ass_reader_running = false;
 		sem_post(&ass_sem);
@@ -265,7 +275,7 @@ extern void getPlayerPts(int64_t *);
 
 void dvbsub_get_stc(int64_t * STC)
 {
-#if HAVE_SH4_HARDWARE // requires libeplayer3
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE // requires libeplayer3
 	if (isEplayer) {
 		getPlayerPts(STC);
 		return;
@@ -323,7 +333,7 @@ static void clear_queue()
 	pthread_mutex_unlock(&packetMutex);
 }
 
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 struct ass_data
 {
 	AVCodecContext *c;
@@ -332,6 +342,7 @@ struct ass_data
 	ass_data(AVCodecContext *_c, AVSubtitle *_sub, int _pid) : c(_c),pid(_pid) { memcpy(&sub, _sub, sizeof(sub));}
 };
 
+extern "C" void dvbsub_ass_clear(void);
 void dvbsub_ass_clear(void)
 {
 	OpenThreads::ScopedLock<OpenThreads::Mutex> m_lock(ass_mutex);
@@ -502,6 +513,7 @@ static void *ass_reader_thread(void *)
 	pthread_exit(NULL);
 }
 
+extern "C" void dvbsub_ass_write(AVCodecContext *c, AVSubtitle *sub, int pid);
 void dvbsub_ass_write(AVCodecContext *c, AVSubtitle *sub, int pid)
 {
 	if (ass_reader_running) {
@@ -514,6 +526,7 @@ void dvbsub_ass_write(AVCodecContext *c, AVSubtitle *sub, int pid)
 
 #endif
 
+extern "C" void dvbsub_write(AVSubtitle *sub, int64_t pts);
 void dvbsub_write(AVSubtitle *sub, int64_t pts)
 {
 	pthread_mutex_lock(&packetMutex);
@@ -583,7 +596,7 @@ static void* reader_thread(void * /*arg*/)
 		pfds[0].events = POLLIN;
 		char _tmp[64];
 
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 		if (isEplayer) {
 			poll(pfds, 1, -1);
 			while (0 > read(pfds[0].fd, _tmp, sizeof(tmp)));
@@ -694,7 +707,7 @@ static void* dvbsub_thread(void* /*arg*/)
 		dvbSubtitleConverter = new cDvbSubtitleConverter;
 
 	int timeout = 1000000;
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 	CFrameBuffer *fb = CFrameBuffer::getInstance();
 	int xres = fb->getScreenWidth(true);
 	int yres = fb->getScreenHeight(true);
@@ -705,7 +718,7 @@ static void* dvbsub_thread(void* /*arg*/)
 #endif
 
 	while(dvbsub_running) {
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 		if (ass_track) {
 			usleep(100000); // FIXME ... should poll instead
 
--- lib/libdvbsub/dvbsub.h
+++ lib/libdvbsub/dvbsub.h
@@ -3,7 +3,7 @@
 int dvbsub_init();
 int dvbsub_stop();
 int dvbsub_close();
-#if HAVE_SH4_HARDWARE
+#if HAVE_SH4_HARDWARE || HAVE_ARM_HARDWARE
 int dvbsub_start(int pid, bool _isEplayer = false);
 #else
 int dvbsub_start(int pid);
