cec2fcb1581e8f5910e07a3a267c3cec8d0d088b
BPanther <bpanther_ts@hotmail.com>
2019-03-18 18:33:41 +0100

    fix tmdb cover in moviebrowser

--- src/gui/moviebrowser/mb.cpp
+++ src/gui/moviebrowser/mb.cpp
@@ -91,6 +91,8 @@ typedef struct dirent64 dirent_struct;
 #define NUMBER_OF_MOVIES_LAST 40 // This is the number of movies shown in last recored and last played list
 #define MOVIE_SMSKEY_TIMEOUT 800
 
+cTmdb* tmdb;
+
 #define MESSAGEBOX_BROWSER_ROW_ITEM_COUNT 22
 const CMenuOptionChooser::keyval MESSAGEBOX_BROWSER_ROW_ITEM[MESSAGEBOX_BROWSER_ROW_ITEM_COUNT] =
 {
@@ -267,6 +269,9 @@ CMovieBrowser::~CMovieBrowser()
 
 	if (m_header)
 		delete m_header;
+
+	if (tmdb)
+		delete tmdb;
 }
 
 void CMovieBrowser::clearListLines()
@@ -423,6 +428,8 @@ void CMovieBrowser::init(void)
 
 	m_doRefresh = false;
 	m_doLoadMovies = false;
+
+	tmdb = cTmdb::getInstance();
 }
 
 void CMovieBrowser::initGlobalSettings(void)
@@ -1973,15 +1980,12 @@ bool CMovieBrowser::onButtonPressMainFrame(neutrino_msg_t msg)
 					extension = "." + extension;
 					str_replace(extension, ".jpg", cover_file);
 					printf("TMDB: %s : %s\n",m_movieSelectionHandler->file.Name.c_str(),cover_file.c_str());
-					cTmdb* tmdb = cTmdb::getInstance();
 					tmdb->setTitle(m_movieSelectionHandler->epgTitle);
 					if ((tmdb->getResults() > 0) && (tmdb->hasCover())) {
 						if (!cover_file.empty())
 							if (tmdb->getSmallCover(cover_file))
 								refresh();
 					}
-					if (tmdb)
-						delete tmdb;
 				}
 			}
 		}
