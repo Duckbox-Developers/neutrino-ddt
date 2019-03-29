6a86b86b686f24c77334d8c1b325344da00619df
vanhofen <vanhofen@gmx.de>
2019-03-30 00:26:48 +0100

    - moviebrowser: reset selected item in MB_FOCUS_LAST_PLAY after deleting any movie

--- src/gui/moviebrowser/mb.cpp
+++ src/gui/moviebrowser/mb.cpp
@@ -928,6 +928,8 @@ int CMovieBrowser::exec(CMenuTarget* parent, const std::string & actionKey)
 				m_doRefresh = true;
 				returnval = menu_return::RETURN_EXIT;
 			}
+			if (m_windowFocus == MB_FOCUS_LAST_PLAY)
+				m_pcLastPlay->setSelectedLine(0);
 		}
 	}
 	return returnval;
@@ -2097,6 +2099,8 @@ bool CMovieBrowser::onButtonPressMainFrame(neutrino_msg_t msg)
 		if (m_movieSelectionHandler != NULL)
 		{
 			onDelete();
+			if (m_windowFocus == MB_FOCUS_LAST_PLAY)
+				m_pcLastPlay->setSelectedLine(0);
 		}
 	}
 	else if (msg == CRCInput::RC_help)
