4a9a95b28c7fc3367402def52c7a7f0b15e721a1
BPanther <bpanther_ts@hotmail.com>
2019-08-17 03:44:56 +0200

    vuzero4k added

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -5247,7 +5247,7 @@ void CNeutrinoApp::loadKeys(const char * fname)
 	g_settings.key_list_end = tconfig->getInt32( "key_list_end", (unsigned int)CRCInput::RC_nokey );
 #if BOXMODEL_HD51 || BOXMODEL_HD60
 	g_settings.key_timeshift = tconfig->getInt32( "key_timeshift", CRCInput::RC_playpause_long ); // FIXME
-#elif BOXMODEL_VUSOLO4K || BOXMODEL_VUDUO4K
+#elif BOXMODEL_VUSOLO4K || BOXMODEL_VUDUO4K || BOXMODEL_VUZERO4K || BOXMODEL_VUDUO
 	g_settings.key_timeshift = tconfig->getInt32( "key_timeshift", CRCInput::RC_playpause );
 #else
 	g_settings.key_timeshift = tconfig->getInt32( "key_timeshift", CRCInput::RC_pause );
@@ -5283,7 +5283,7 @@ void CNeutrinoApp::loadKeys(const char * fname)
 #if BOXMODEL_HD51 || BOXMODEL_HD60
 	g_settings.mpkey_play = tconfig->getInt32( "mpkey.play", CRCInput::RC_playpause );
 	g_settings.mpkey_pause = tconfig->getInt32( "mpkey.pause", CRCInput::RC_playpause );
-#elif BOXMODEL_VUSOLO4K || BOXMODEL_VUDUO4K
+#elif BOXMODEL_VUSOLO4K || BOXMODEL_VUDUO4K || BOXMODEL_VUZERO4K || BOXMODEL_VUDUO
 	g_settings.mpkey_play = tconfig->getInt32( "mpkey.play", CRCInput::RC_play );
 	g_settings.mpkey_pause = tconfig->getInt32( "mpkey.pause", CRCInput::RC_playpause );
 #else
