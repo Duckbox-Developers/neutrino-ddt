3f077007b907d56bd23ce8ef6361de81502b4f7d
BPanther <bpanther_ts@hotmail.com>
2019-09-28 21:25:13 +0200

    crypt info verimatrix added

--- src/nhttpd/tuxboxapi/neutrinoapi.cpp
+++ src/nhttpd/tuxboxapi/neutrinoapi.cpp
@@ -504,8 +504,9 @@ std::string CNeutrinoAPI::getAudioInfoAsString(void)
 std::string CNeutrinoAPI::getCryptInfoAsString(void)
 {
 	std::stringstream out;
-	std::string casys[11]=	{"Irdeto:","Betacrypt:","Seca:","Viaccess:","Nagra:","Conax: ","Cryptoworks:","Videoguard:","EBU:","XCrypt:","PowerVU:"};
-	int caids[] =		{ 0x600, 0x1700, 0x0100, 0x0500, 0x1800, 0xB00, 0xD00, 0x900, 0x2600, 0x4a00, 0x0E00 };
+	std::string casys[13]=	{"Irdeto:","Betacrypt:","Seca:","Viaccess:","Nagra:","Conax:","Cryptoworks:","Videoguard:","Biss:","XCrypt:","PowerVU:","Tandberg:","Verimatrix:"};
+
+	int caids[] =		{ 0x600, 0x1700, 0x0100, 0x0500, 0x1800, 0xB00, 0xD00, 0x900, 0x2600, 0x4a00, 0x0E00, 0x1000, 0x5600 };
 
 	OpenThreads::ScopedPointerLock<OpenThreads::Mutex> lock(pmutex);
 	CZapitChannel * channel = CZapit::getInstance()->GetCurrentChannel();
