d980f74ccb62ed27c1c1d8a6984faf90ed2e240f
BPanther <bpanther_ts@hotmail.com>
2019-11-09 20:27:59 +0100

    h7 syntax fix

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -5295,7 +5295,7 @@ void CNeutrinoApp::loadKeys(const char * fname)
 	g_settings.mpkey_rewind = tconfig->getInt32( "mpkey.rewind", CRCInput::RC_rewind );
 	g_settings.mpkey_forward = tconfig->getInt32( "mpkey.forward", CRCInput::RC_forward );
 	g_settings.mpkey_stop = tconfig->getInt32( "mpkey.stop", CRCInput::RC_stop );
-#if BOXMODEL_HD51 || BOXMDOELL_H7
+#if BOXMODEL_HD51 || BOXMODEL_H7
 	g_settings.mpkey_play = tconfig->getInt32( "mpkey.play", CRCInput::RC_playpause );
 	g_settings.mpkey_pause = tconfig->getInt32( "mpkey.pause", CRCInput::RC_playpause );
 #elif BOXMODEL_VUSOLO4K || BOXMODEL_VUDUO4K || BOXMODEL_VUULTIMO4K || BOXMODEL_VUZERO4K || BOXMODEL_VUDUO || BOXMODEL_VUUNO4KSE || BOXMODEL_VUUNO4K
