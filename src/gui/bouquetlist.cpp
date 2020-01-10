cf2dad4f4e1f536b321ac2792bafb0b9add9617a
MarkusVolk <f_l_k@t-online.de>
2020-01-10 06:42:12 +0100

    bouquetlist.cpp: increase destination array to avoid possible truncation
    Signed-off-by: MarkusVolk <f_l_k@t-online.de>

--- src/gui/bouquetlist.cpp
+++ src/gui/bouquetlist.cpp
@@ -677,7 +677,7 @@ void CBouquetList::paintItem(int pos)
 	}
 
 	if (npos < (int) Bouquets.size()) {
-		char num[10];
+		char num[12];
 		sprintf((char*) num, "%d", npos + 1);
 		int iw = 0, ih = 0;
 		if ((g_settings.epg_scan == CEpgScan::SCAN_SEL) &&
