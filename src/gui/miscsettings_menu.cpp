369eaa9ce48d906f5039a45ee4a882ce24660d80
DboxOldie <>
2020-02-18 10:20:08 +0100

    add read epg to epg function

--- src/gui/miscsettings_menu.cpp
+++ src/gui/miscsettings_menu.cpp
@@ -159,6 +159,7 @@ int CMiscMenue::exec(CMenuTarget* parent, const std::string &actionKey)
 		{
 			printf("Reading epg cache from %s ...\n", g_settings.epg_dir.c_str());
 			g_Sectionsd->readSIfromXML(g_settings.epg_dir.c_str());
+			return menu_return::RETURN_EXIT_ALL;
 		}
 
 		for (std::list<std::string>::iterator it = g_settings.xmltv_xml.begin(); it != g_settings.xmltv_xml.end(); ++it)
