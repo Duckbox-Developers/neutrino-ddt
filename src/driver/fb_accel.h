cc826d49f9c5e1d1a49298be7805ebb608ca0a74
Jacek Jendrzej <overx300@gmail.com>
2020-04-19 15:10:14 +0200

    disable CFbAccelARM::paintRect for VU+ &co, need fix

--- src/driver/fb_accel.h
+++ src/driver/fb_accel.h
@@ -224,7 +224,9 @@ class CFbAccelARM
 		void setOsdResolutions();
 		void set3DMode(Mode3D);
 		Mode3D get3DMode(void);
+#if BOXMODEL_HD51
 		void paintRect(const int x, const int y, const int dx, const int dy, const fb_pixel_t col);
+#endif
 };
 
 class CFbAccelMIPS
