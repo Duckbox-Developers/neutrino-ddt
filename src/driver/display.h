7d57d7eaad3748cfb63c0cf27c16668fe2df14bc
BPanther <bpanther_ts@hotmail.com>
2021-02-15 01:20:11 +0100

    dm8000 lcd driver added

--- src/driver/display.h
+++ src/driver/display.h
@@ -3,7 +3,12 @@
 #include <driver/vfd.h>
 #endif
 #if HAVE_SPARK_HARDWARE || HAVE_GENERIC_HARDWARE  || HAVE_ARM_HARDWARE || HAVE_MIPS_HARDWARE
+#if BOXMODEL_DM8000
+#include <driver/lcdd.h>
+#define CVFD CLCD
+#else
 #include <driver/simple_display.h>
+#endif
 #endif
 #ifdef ENABLE_GRAPHLCD
 #include <driver/nglcd.h>
