8d0ded67711e6c9be9d7dd046623a3708fab241f
svenhoefer <svenhoefer@svenhoefer.com>
2021-04-05 23:23:51 +0200

    fix fading

--- src/driver/fb_accel.h
+++ src/driver/fb_accel.h
@@ -128,6 +128,8 @@ class CFbAccelARM
 		void setOsdResolutions();
 		void set3DMode(Mode3D);
 		Mode3D get3DMode(void);
+		void setBlendMode(uint8_t mode);
+		void setBlendLevel(int level);
 #if ENABLE_ARM_ACC
 		void paintRect(const int x, const int y, const int dx, const int dy, const fb_pixel_t col);
 #endif
