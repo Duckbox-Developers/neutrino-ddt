d6dcd1c53ee810a8a605421acace361a445dba5e
Frankenstone <dampf_acc@online.de>
2021-04-29 18:23:05 +0200

    locals: remove
    - LOCALE_OPTIONS_ON_WITHOUT_MESSAGES
    - options.on.without_messages

--- src/system/locals_intern.h
+++ src/system/locals_intern.h
@@ -2115,7 +2115,6 @@ const char * locale_real_names[] =
 	"options.ntp_on",
 	"options.off",
 	"options.on",
-	"options.on.without_messages",
 	"osd.preset_screen_a",
 	"osd.preset_screen_b",
 	"parentallock.bouquetmode",
