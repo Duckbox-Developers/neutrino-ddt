97cc9fb3cd8cee6a40018a7595e1c25ac0d95adf
GetAway <get-away@t-online.de>
2021-05-17 13:31:51 +0200

    fade.cpp: fix flickering after fade out

--- src/driver/fade.cpp
+++ src/driver/fade.cpp
@@ -81,10 +81,9 @@ void COSDFader::StopFade()
 {
 	if ( fadeIn || fadeOut ) {
 		g_RCInput->killTimer(fadeTimer);
+		usleep(FADE_TIME*10);
+
 		frameBuffer->setBlendMode(1); // Global alpha multiplied with pixel alpha
-#if HAVE_SH4_HARDWARE
-		usleep(60000);
-#endif
 		fadeIn = fadeOut = false;
 	}
 }
