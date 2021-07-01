691e1f6561f1524433bbd4c33bfb24181caca0aa
Frankenstone <dampf_acc@online.de>
2021-07-01 16:33:15 +0200

    Revert "ScanSDT add extended menu option"
    This reverts commit bf9ce5efe94b19431afe014c9286085682ec6a22.

--- src/zapit/src/zapit.cpp
+++ src/zapit/src/zapit.cpp
@@ -2821,7 +2821,7 @@ void CZapitSdtMonitor::run()
 			bool updated = CServiceManager::getInstance()->SaveCurrentServices(tpid);
 			CServiceManager::getInstance()->CopyCurrentServices(tpid);
 
-			if(updated && (CZapit::getInstance()->GetScanSDT()))
+			if(updated && (CZapit::getInstance()->GetScanSDT() == 1))
 				CZapit::getInstance()->SendEvent(CZapitClient::EVT_SDT_CHANGED);
 			if(!updated)
 				printf("[sdt monitor] no changes.\n");
