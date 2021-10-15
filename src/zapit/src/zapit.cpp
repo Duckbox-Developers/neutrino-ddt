c3c0eb22d78419f8c428652cd7d16dcd32d7dab5
BPanther <bpanther_ts@hotmail.com>
2021-10-15 07:20:12 +0200

    more pip changes

--- src/zapit/src/zapit.cpp
+++ src/zapit/src/zapit.cpp
@@ -95,6 +95,8 @@ extern cDemux *videoDemux;
 #ifdef ENABLE_PIP
 extern cVideo *pipVideoDecoder[3];
 extern cDemux *pipVideoDemux[3];
+extern cAudio * pipAudioDecoder[3];
+extern cDemux * pipAudioDemux[3];
 #endif
 
 cDemux *pcrDemux = NULL;
@@ -686,6 +688,8 @@ bool CZapit::StopPip(int pip)
 		CCamManager::getInstance()->Stop(pip_channel_id[pip], CCamManager::PIP);
 		pipVideoDemux[pip]->Stop();
 		pipVideoDecoder[pip]->Stop();
+		pipAudioDemux[pip]->Stop();
+		pipAudioDecoder[pip]->Stop();
 		pip_fe[pip] = NULL;
 		pip_channel_id[pip] = 0;
 		return true;
@@ -764,6 +768,21 @@ bool CZapit::StartPip(const t_channel_id channel_id, int pip)
 
 	pipVideoDecoder[pip]->Pig(g_settings.pip_x,g_settings.pip_y,g_settings.pip_width,g_settings.pip_height,g_settings.screen_width,g_settings.screen_height);
 	pipVideoDecoder[pip]->ShowPig(1);
+
+	if (!pipAudioDemux[pip]) {
+		pipAudioDemux[pip] = new cDemux(dnum);
+		pipAudioDemux[pip]->Open(DMX_AUDIO_CHANNEL);
+		if (!pipAudioDecoder[pip]) {
+			pipAudioDecoder[pip] = new cAudio(0, NULL, NULL, dnum);
+		}
+	}
+
+	if (newchannel->getAudioChannel())
+		pipAudioDecoder[pip]->SetStreamType(newchannel->getAudioChannel()->audioChannelType);
+	pipAudioDemux[pip]->pesFilter(newchannel->getAudioPid());
+
+	pipVideoDecoder[pip]->SetSyncMode((AVSYNC_TYPE) g_settings.avsync);
+	pipAudioDecoder[pip]->SetSyncMode((AVSYNC_TYPE) g_settings.avsync);
 
 	CCamManager::getInstance()->Start(newchannel->getChannelID(), CCamManager::PIP);
 	return true;
@@ -2500,6 +2519,7 @@ bool CZapit::Start(Z_start_arg *ZapStart_arg)
 		{
 			pipVideoDecoder[i] = new cVideo(0, NULL, NULL, i+1);
 			pipVideoDecoder[i]->ShowPig(0);
+			pipAudioDecoder[i] = new cAudio(0, NULL, NULL, i+1);
 		}
 	}
 #endif
@@ -2707,6 +2727,10 @@ void CZapit::run()
 			pipVideoDecoder[i] = NULL;
 		if (pipVideoDemux[i])
 			pipVideoDemux[i] = NULL;
+		if (pipAudioDecoder[i])
+			pipAudioDecoder[i] = NULL;
+		if (pipAudioDemux[i])
+			pipAudioDemux[i] = NULL;
 	}
 #endif
 
