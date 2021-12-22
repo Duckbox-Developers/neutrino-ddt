f3e89ad45930e258c681d8b3a3543f487e611185
BPanther <bpanther_ts@hotmail.com>
2021-12-22 07:21:19 +0100

    fix vuduo

--- src/zapit/src/frontend.cpp
+++ src/zapit/src/frontend.cpp
@@ -299,11 +299,13 @@ void CFrontend::getFEInfo(void)
 				fe_can_multistream = info.caps & FE_CAN_MULTISTREAM;
 				printf("[fe%d/%d] add delivery system DVB-S2 (delivery_system: %d / Multistream: %s)\n", adapter, fenumber, (fe_delivery_system_t)prop[0].u.buffer.data[i], fe_can_multistream ? "yes" :"no");
 				break;
+#if !defined (HAVE_SH4_HARDWARE) && !defined (HAVE_MIPS_HARDWARE)
 			case SYS_DVBS2X:
 				deliverySystemMask |= DVB_S2X;
 				fe_can_multistream = info.caps & FE_CAN_MULTISTREAM;
 				printf("[fe%d/%d] add delivery system DVB-S2X (delivery_system: %d / Multistream: %s)\n", adapter, fenumber, (fe_delivery_system_t)prop[0].u.buffer.data[i], fe_can_multistream ? "yes" :"no");
 				break;
+#endif
 			case SYS_DTMB:
 				deliverySystemMask |= DTMB;
 				printf("[fe%d/%d] add delivery system DTMB (delivery_system: %d)\n", adapter, fenumber, (fe_delivery_system_t)prop[0].u.buffer.data[i]);
