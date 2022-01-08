fa4f005efbd10c239ae9ef8ffd2a95696e6fa24f
Frankenstone <dampf_acc@online.de>
2022-01-08 10:48:33 +0100

    - src/eitd/sectionsd: update unixtime to 01.01.2022 00:00:00

--- src/eitd/sectionsd.cpp
+++ src/eitd/sectionsd.cpp
@@ -1616,8 +1616,8 @@ void CTimeThread::run()
 				if (first_time)
 					sleep_time = 5;
 			}
-			/* in case of wrong TDT date, dont send time is set, 1609455600 - 01.01.2021 00:00:00 */
-			if (time_ntp || (dvb_time > (time_t) 1609455600))
+			/* in case of wrong TDT date, dont send time is set, 1640991600 - 01.01.2022 00:00:00 */
+			if (time_ntp || (dvb_time > (time_t) 1640991600))
 			{
 				sendTimeEvent(time_ntp, dvb_time);
 				debug(DEBUG_ERROR, "%s: Time set via %s, going to sleep for %d seconds.", name.c_str(),
