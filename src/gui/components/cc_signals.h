74d3912ba06ed4fce3185a6cbc14489ab6aaa509
Frankenstone <dampf_acc@online.de>
2022-09-27 14:56:41 +0200

    Revert "cc_signals: add signals for kill()"
    This reverts commit e5d0138ce65580f9971b81b11c253f60e65cebb6.

--- src/gui/components/cc_signals.h
+++ src/gui/components/cc_signals.h
@@ -124,11 +124,6 @@ class CComponentsSignals : public sigc::trackable
 		///signal on after execute hide()
 		sigc::signal<void> OnAfterHide;
 
-		///signal on before execute kill()
-		sigc::signal<void> OnBeforeKill;
-		///signal on after execute kill()
-		sigc::signal<void> OnAfterKill;
-
 		///signal on CComponentsForm::setSelectedItem() is completed
 		sigc::signal<void> OnSelect;
 };
