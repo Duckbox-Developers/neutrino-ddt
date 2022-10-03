bf4c8cd7466c5ced4b1ee819506cfe87366a5c6b
Frankenstone <dampf_acc@online.de>
2022-10-03 21:52:19 +0200

    Revert "reverting neutrino streaming to old behavior, raw streaming only for e2 notating supported"
    This reverts commit ba3a3593543057175c96697c2a53a9a85f1d36c8.

--- src/driver/streamts.cpp
+++ src/driver/streamts.cpp
@@ -198,7 +198,7 @@ void CStreamInstance::run()
 		dmx->addPid(*it);
 	}
 
-	dmx->Start();
+	dmx->Start(true);
 
 	if (!send_raw)
 		CCamManager::getInstance()->Start(channel_id, CCamManager::STREAM);
@@ -395,7 +395,6 @@ bool CStreamManager::Parse(int fd, stream_pids_t &pids, t_channel_id &chid, CFro
 {
 	char cbuf[512];
 	char *bp;
-	send_raw = false;
 
 	FILE *fp = fdopen(fd, "r+");
 	if (fp == NULL)
@@ -444,19 +443,22 @@ bool CStreamManager::Parse(int fd, stream_pids_t &pids, t_channel_id &chid, CFro
 
 	t_channel_id tmpid = 0;
 	bp = &cbuf[5];
-
-	if (strstr(bp,"id="))
+	if (sscanf(bp, "id=%" SCNx64, &tmpid) == 1)
 	{
-		if (sscanf(bp, "id=%" SCNx64, &tmpid) == 1) {
-			channel = CServiceManager::getInstance()->FindChannel(tmpid);
-			chid = tmpid;
-			send_raw = false;
-			pids.clear(); // to catch and stream all pids later !
-		}
+		channel = CServiceManager::getInstance()->FindChannel(tmpid);
+		chid = tmpid;
+	}
+	int tmpraw = 0;
+	bp = &cbuf[25];
+
+	if (sscanf(bp, "raw=%d", &tmpraw) == 1)
+	{
+		send_raw = (tmpraw > 0);
 	}
 
-	else
+	if (!tmpid)
 	{
+		bp = &cbuf[5];
 		u_int service;
 		u_int i1, i2, i3, i4, satpos;
 
@@ -469,17 +471,22 @@ bool CStreamManager::Parse(int fd, stream_pids_t &pids, t_channel_id &chid, CFro
 			{
 				printf("e2 -> n chid:%" SCNx64 "\n", tmp_channel->getChannelID());
 				channel = tmp_channel;
-				chid = channel->getChannelID();
+				chid = tmp_channel->getChannelID();
 				send_raw = true;
-				pids.clear(); // to catch and stream all pids later !
 			}
 		}
 	}
-
 	if (!channel)
 		return false;
 
 	printf("CStreamManager::Parse: channel_id %" PRIx64 " [%s] send %s\n", chid, channel->getName().c_str(), send_raw ? "raw" : "decrypted");
+
+	streammap_iterator_t it = streams.find(chid);
+	if (it != streams.end())
+	{
+		printf("CStreamManager::Parse: channel_id %" PRIx64 " already streaming, just add client %d\n", chid, fd);
+		return true;
+	}
 
 	if (IS_WEBCHAN(chid))
 		return true;
@@ -559,8 +566,9 @@ bool CStreamManager::AddClient(int connfd)
 	stream_pids_t pids;
 	t_channel_id channel_id;
 	CFrontend *frontend;
+	bool send_raw;
 
-	bool send_raw = false;
+	pids.clear(); // to catch and stream all pids later !
 
 	if (Parse(connfd, pids, channel_id, frontend, send_raw)) {
 		OpenThreads::ScopedLock<OpenThreads::Mutex> m_lock(mutex);
@@ -666,11 +674,13 @@ void CStreamManager::run()
 						perror("CStreamManager::run(): accept");
 						continue;
 					}
-#if 0
+
+					g_RCInput->postMsg(NeutrinoMessages::EVT_STREAM_START, 0);
 					if (!AddClient(connfd))
+					{
 						close(connfd);
-#endif
-					g_RCInput->postMsg(NeutrinoMessages::EVT_STREAM_START, connfd);
+						g_RCInput->postMsg(NeutrinoMessages::EVT_STREAM_STOP, 0);
+					}
 					poll_timeout = 1000;
 				}
 				else
