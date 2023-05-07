63b94c61786186f4d01aabc63234c8b3a349ab39
BPanther <bpanther_ts@hotmail.com>
2023-05-07 09:30:38 +0200

    read epg from xml (thx vanhofen & TangoCash)

--- src/gui/webtv_setup.cpp
+++ src/gui/webtv_setup.cpp
@@ -208,7 +208,7 @@ int CWebTVSetup::Show()
 			g_settings.webtv_xml.push_back(f->getName());
 		}
 		g_Zapit->reinitChannels();
-		CNeutrinoApp::getInstance()->xmltv_xml_m3u_readepg();
+		CNeutrinoApp::getInstance()->xmltv_xml_auto_readepg();
 		changed = false;
 		hint.hide();
 	}
