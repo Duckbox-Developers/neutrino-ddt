42ccafbcb43af9d44340a11b53ca0fb2d31894e8
BPanther <bpanther_ts@hotmail.com>
2023-09-03 22:36:01 +0200

    movieplayer: rework player handling (thx dbt1)

--- src/gui/movieplayer.h
+++ src/gui/movieplayer.h
@@ -275,7 +275,7 @@ class CMoviePlayerGui : public CMenuTarget
 	void UpdatePosition();
 	tshift_mode timeshift;
 	int file_prozent;
-	cPlayback *getPlayback() { return playback; }
+	static cPlayback *getPlayback();
 	void SetFile(std::string &name, std::string &file, std::string info1="", std::string info2="", std::string file2="") { pretty_name = name; file_name = file; info_1 = info1; info_2 = info2; second_file_name = file2; }
 	unsigned int getAPID(void);
 	unsigned int getAPID(unsigned int i);
