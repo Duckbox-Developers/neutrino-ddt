4f2a3d16acadbc14972cbff1467a4e1868bd672a
BPanther <bpanther_ts@hotmail.com>
2024-05-28 11:38:36 +0200

    dm820: fix lcd

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -4350,7 +4350,11 @@ void CNeutrinoApp::saveEpg(int _mode)
 		}
 		CVFD::getInstance()->Clear();
 		CVFD::getInstance()->setMode(CVFD::MODE_TVRADIO);
+#if BOXMODEL_DM820
+		CVFD::getInstance()->setMode(CVFD::MODE_MENU_UTF8, g_Locale->getText(LOCALE_EPG_SAVING));
+#else
 		CVFD::getInstance()->ShowText(g_Locale->getText(LOCALE_EPG_SAVING));
+#endif
 
 		printf("[neutrino] Saving EPG to %s...\n", g_settings.epg_dir.c_str());
 		g_Sectionsd->writeSI2XML(g_settings.epg_dir.c_str());
@@ -4494,7 +4498,11 @@ void CNeutrinoApp::standbyMode(bool bOnOff, bool fromDeepStandby)
 #ifdef ENABLE_GRAPHLCD
 		nGLCD::StandbyMode(true);
 #endif
+#if BOXMODEL_DM820
+		CVFD::getInstance()->setMode(CVFD::MODE_MENU_UTF8, "standby...");
+#else
 		CVFD::getInstance()->ShowText("standby...");
+#endif
 		if( mode == NeutrinoModes::mode_avinput ) {
 		}
 		g_InfoViewer->setUpdateTimer(0); // delete timer
@@ -4572,7 +4580,11 @@ void CNeutrinoApp::standbyMode(bool bOnOff, bool fromDeepStandby)
 		// Active standby off
 		powerManager->SetStandby(false, false);
 		CVFD::getInstance()->setMode(CVFD::MODE_TVRADIO);
+#if BOXMODEL_DM820
+		CVFD::getInstance()->setMode(CVFD::MODE_MENU_UTF8, "resume");
+#else
 		CVFD::getInstance()->ShowText("resume");
+#endif
 		videoDecoder->Standby(false);
 		CEpgScan::getInstance()->Stop();
 		CSectionsdClient::CurrentNextInfo dummy;
@@ -5040,7 +5052,11 @@ void stop_daemons(bool stopall, bool for_flash)
 	if (for_flash) {
 		CVFD::getInstance()->Clear();
 		CVFD::getInstance()->setMode(CVFD::MODE_TVRADIO);
+#if BOXMODEL_DM820
+		CVFD::getInstance()->setMode(CVFD::MODE_MENU_UTF8, "Stop daemons...");
+#else
 		CVFD::getInstance()->ShowText("Stop daemons...");
+#endif
 		g_settings.epg_scan_mode = CEpgScan::MODE_OFF;
 		exec_controlscript(NEUTRINO_ENTER_FLASH_SCRIPT);
 	}
@@ -5620,7 +5636,11 @@ void CNeutrinoApp::CheckFastScan(bool standby, bool reload)
 		}
 		if (standby || (new_fst != scansettings.fst_version)) {
 			CVFD::getInstance()->setMode(CVFD::MODE_TVRADIO);
+#if BOXMODEL_DM820
+			CVFD::getInstance()->setMode(CVFD::MODE_MENU_UTF8, g_Locale->getText(LOCALE_SATSETUP_FASTSCAN_HEAD));
+#else
 			CVFD::getInstance()->ShowText(g_Locale->getText(LOCALE_SATSETUP_FASTSCAN_HEAD));
+#endif
 			CHintBox * fhintbox = NULL;
 			if (!standby) {
 				fhintbox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SATSETUP_FASTSCAN_HEAD));
