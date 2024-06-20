0110692b770edfdec292d062c5bcc0e26b545da0
BPanther <bpanther_ts@hotmail.com>
2024-06-21 00:48:35 +0200

    fix dm rc

--- src/nhttpd/tuxboxapi/controlapi.cpp
+++ src/nhttpd/tuxboxapi/controlapi.cpp
@@ -66,15 +66,11 @@ extern CBouquetManager *g_bouquetManager;
 #define RC_DEVICE "/dev/input/event0"
 #define RC_DEVICE_FALLBACK "/dev/input/event1"
 
-#elif BOXMODEL_H7 || BOXMODEL_DM8000
+#elif BOXMODEL_H7
 #define RC_DEVICE "/dev/input/event2"
 #define RC_DEVICE_FALLBACK "/dev/input/event1"
 
-//#elif BOXMODEL_DM820
-//#define RC_DEVICE "/dev/input/event3"
-//#define RC_DEVICE_FALLBACK "/dev/input/event0"
-
-#elif BOXMODEL_DM7080 || BOXMODEL_DM820
+#elif BOXMODEL_DM8000 || BOXMODEL_DM7080 || BOXMODEL_DM820 || BOXMODEL_DM900 || BOXMODEL_DM920
 #define RC_DEVICE "/dev/input/event1"
 #define RC_DEVICE_FALLBACK "/dev/input/event4"
 
