5620c301beee152302658366e1aa3e97ef03a237
BPanther <bpanther_ts@hotmail.com>
2024-08-06 06:41:45 +0200

    dm800se/dm800sev2 added

--- src/gui/bookmarkmanager.cpp
+++ src/gui/bookmarkmanager.cpp
@@ -202,7 +202,7 @@ void CBookmarkManager::paintItem(int pos)
 		// LCD Display
 		if (i_selected)
 		{
-#if BOXMODEL_DM820 || BOXMODEL_DM7080 || BOXMODEL_DM8000 || BOXMODEL_DM7020HD
+#if BOXMODEL_DM820 || BOXMODEL_DM7080 || BOXMODEL_DM8000 || BOXMODEL_DM7020HD || BOXMODEL_DM800SE || BOXMODEL_DM800SEV2
 			CVFD::getInstance()->setMode(CVFD::MODE_MENU_UTF8, theBookmark.getName());
 #else
 			CVFD::getInstance()->showMenuText(0, theBookmark.getName(), -1, true); // UTF-8
